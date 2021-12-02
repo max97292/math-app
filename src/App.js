8e1d037698332d1c289799435d09e73daf3125f4
Maksym Diachenko <max97292@gmail.com>
2021-12-02 10:10:03 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* math card */}
+      <div className="math-card">
+        <div className="spot">1</div>
+        <div className="spot">1</div>
+        <div className="spot">+</div>
+        <div className="total">2</div>
+      </div>
+
+      <div>
+        <div className="cards numbers">
+          {Array(10)
+            .fill(0)
+            .map((n, i) => (
+              <div key={i} className="card">
+                {i + 1}
+              </div>
+            ))}
+        </div>
+
+        <div className="cards operators">
+          {['*', '-', '+', '/'].map((o, i) => (
+            <div key={i} className="card">
+              {o}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
