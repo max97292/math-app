39d61e64aeb6af27585095903a7c6f111716cf77
Maksym Diachenko <max97292@gmail.com>
2021-12-02 12:31:21 +0200

    Create logic to drag & drop items

--- src/App.js
+++ src/App.js
@@ -1,36 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './App.css';
 
 export default function App() {
+  const [number1, setNumber1] = useState(1);
+  const [number2, setNumber2] = useState(3);
+  const [operator, setOperator] = useState('*');
+
+  function handleDrop(spot, item) {
+    if (spot === 'number1') setNumber1(item.text);
+    if (spot === 'number2') setNumber2(item.text);
+    if (spot === 'operator') setOperator(item.text);
+  }
+
   return (
-    <div className="app">
-      {/* math card */}
-      <div className="math-card">
-        <div className="spot">1</div>
-        <div className="spot">1</div>
-        <div className="spot">+</div>
-        <div className="total">2</div>
-      </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className='app'>
+        {/* math card */}
+        <div className='math-card'>
+          <Spot
+            type='number'
+            text={number1}
+            spot='number1'
+            handleDrop={handleDrop}
+          />
+          <Spot
+            type='number'
+            text={number2}
+            spot='number2'
+            handleDrop={handleDrop}
+          />
+          <Spot
+            type='operator'
+            text={operator}
+            spot='operator'
+            handleDrop={handleDrop}
+          />
+          <div className='total'>
+            {isFinite(eval(`${number1}${operator}${number2}`))
+              ? eval(`${number1}${operator}${number2}`)
+              : '∞'}
+          </div>
+        </div>
 
-      <div>
-        <div className="cards numbers">
-          {Array(10)
-            .fill(0)
-            .map((n, i) => (
-              <div key={i} className="card">
-                {i + 1}
-              </div>
+        <div>
+          <div className='cards numbers'>
+            {Array(10)
+              .fill(0)
+              .map((n, i) => (
+                <Card key={i} type='number' text={i} />
+              ))}
+          </div>
+
+          <div className='cards operators'>
+            {['*', '-', '+', '/'].map((o, i) => (
+              <Card key={i} type='operator' text={o} />
             ))}
-        </div>
-
-        <div className="cards operators">
-          {['*', '-', '+', '/'].map((o, i) => (
-            <div key={i} className="card">
-              {o}
-            </div>
-          ))}
+          </div>
         </div>
       </div>
+    </DndProvider>
+  );
+}
+
+function Spot({ type, text, spot, handleDrop }) {
+  const [{ canDrop, isOver }, dropRef] = useDrop({
+    accept: type,
+    drop: (item) => {
+      // here is where we do the update
+      handleDrop(spot, item);
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  let backgroundColor = '#f2f2f2';
+  if (canDrop) backgroundColor = '#3db897';
+  if (isOver) backgroundColor = '#4bdcb5';
+
+  return (
+    <div className='spot' ref={dropRef} style={{ backgroundColor }}>
+      {text}
+    </div>
+  );
+}
+
+function Card({ type, text }) {
+  const [{ opacity }, dragRef] = useDrag({
+    type,
+    item: { type, text },
+    collect: (monitor) => ({
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+  });
+
+  return (
+    <div className='card' ref={dragRef} style={{ opacity }}>
+      {text}
     </div>
   );
 }
